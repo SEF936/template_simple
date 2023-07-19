22b71114e47670ad82a1d564ac8ea0c7a7d8464c
Vandanjon <benoit@vandanjon.fr>
2023-07-19 16:22:01 +0200

    add frontend routes

---
+++ frontend/pages/Element.jsx
@@ -0,0 +1,11 @@
+const Element = () => {
+	return (
+		<div id="ElementContainer">
+			<h1>Element</h1>
+
+			<p>Element Page</p>
+		</div>
+	);
+};
+
+export default Element;
---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+	return (
+		<div id="HomeContainer">
+			<h1>Home</h1>
+
+			<p>Home Page</p>
+		</div>
+	);
+};
+
+export default Home;
