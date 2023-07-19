64a4c8d2919017b7cf9feb93e079772808992f54
Vandanjon <benoit@vandanjon.fr>
2023-07-19 23:01:59 +0200

    add icon link in database samples

--- backend/database.sql
+++ backend/database.sql
@@ -4,12 +4,14 @@ CREATE TABLE `timeline` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `start_date` DATE NOT NULL,
   `end_date` DATE,
-  `event` VARCHAR(80) NOT NULL
+  `event_icon` VARCHAR(80),
+  `event_name` VARCHAR(80) NOT NULL,
+  `event_description` TEXT
 ) ENGINE=InnoDB;
 
 LOCK TABLES `timeline` WRITE;
-INSERT INTO `timeline` (`start_date`, `end_date`, `event`) VALUES
-("1993-01-01", null, "HTML"),
-("1996-05-01", null, "JS"),
-("1996-12-01", null, "CSS");
+INSERT INTO `timeline` (`start_date`, `event_icon`, `event_name`) VALUES
+("1993-01-01", "https://cdn-icons-png.flaticon.com/512/186/186320.png", "HTML"),
+("1996-05-01", "https://cdn-icons-png.flaticon.com/512/136/136530.png", "JS"),
+("1996-12-01", "https://cdn-icons-png.flaticon.com/512/186/186319.png", "CSS");
 UNLOCK TABLES;
