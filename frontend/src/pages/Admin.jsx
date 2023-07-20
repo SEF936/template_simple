f0127e22bfebfd036b6ecffca1895d94b273ff41
Vandanjon <benoit@vandanjon.fr>
2023-07-20 12:24:09 +0200

    add icons for crud operations

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,11 +1,53 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import instance from "../helpers/instance";
 
 const Admin = () => {
+	const [events, setEvents] = useState();
+
+	useEffect(() => {
+		instance
+			.get("/timeline")
+			.then((res) => setEvents(res.data))
+			.catch((err) => console.error(err));
+	}, []);
+
+	console.log(events);
+
 	return (
 		<div id="AdminContainer">
 			<h1>Admin</h1>
 
-			<p>Admin Page</p>
+			{events ? (
+				events.map((ev) => (
+					<article key={ev.id}>
+						<ul>
+							<li>
+								<input placeholder={ev.id} />
+								<input placeholder={ev.start_date} />
+								<input placeholder={ev.end_date} />
+								<input placeholder={ev.event_name} />
+								<input placeholder={ev.event_icon} />
+								<input placeholder={ev.event_description} />
+							</li>
+						</ul>
+
+						<div className="icon-container">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
+								alt=""
+							/>
+
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/1250/1250925.png"
+								alt=""
+							/>
+						</div>
+					</article>
+				))
+			) : (
+				<p>Loading ...</p>
+			)}
 
 			<Link to="/">
 				<button>Retour à l'accueil</button>
