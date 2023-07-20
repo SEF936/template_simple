6b556702a65a23e69d61642b7caf5513b828ddcb
Vandanjon <benoit@vandanjon.fr>
2023-07-20 10:12:45 +0200

    add button from admin to home

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Admin = () => {
 	return (
 		<div id="AdminContainer">
 			<h1>Admin</h1>
 
 			<p>Admin Page</p>
+
+			<Link to="/">
+				<button>Retour à l'accueil</button>
+			</Link>
 		</div>
 	);
 };
