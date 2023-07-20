1d4e3933fc34bc83e7a6a6334fb8f7659d9b4411
Vandanjon <benoit@vandanjon.fr>
2023-07-20 12:57:45 +0200

    style admin page

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -23,23 +23,39 @@ const Admin = () => {
 					<article key={ev.id}>
 						<ul>
 							<li>
+								<span>ID</span>
 								<input placeholder={ev.id} />
+							</li>
+							<li>
+								<span>Start Date</span>
 								<input placeholder={ev.start_date} />
+							</li>
+							<li>
+								<span>End Date</span>
 								<input placeholder={ev.end_date} />
+							</li>
+							<li>
+								<span>Event Name</span>
 								<input placeholder={ev.event_name} />
+							</li>
+							<li>
+								<span>Event Icon URL</span>
 								<input placeholder={ev.event_icon} />
+							</li>
+							<li>
+								<span>Event Description</span>
 								<input placeholder={ev.event_description} />
 							</li>
 						</ul>
 
 						<div className="icon-container">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
+								src="https://cdn-icons-png.flaticon.com/512/1250/1250925.png"
 								alt=""
 							/>
 
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1250/1250925.png"
+								src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
 								alt=""
 							/>
 						</div>
